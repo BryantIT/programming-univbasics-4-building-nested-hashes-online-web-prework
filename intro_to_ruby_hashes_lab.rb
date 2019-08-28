9ca3cad7bf28b9fd7f07edd91499783b3c04689b
Bryant Richards <dragonseyeimages@gmail.com>
2019-08-28 23:47:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,13 +1,13 @@
 def base_hash
   monopoly = {
-    :railroads => {},
+    :railroads => {}
   }
 end
 
 def monopoly_with_second_tier
   monopoly = {
     :railroads => {
-      :pieces => {},
+      :pieces => {}
   }
 end
 
