4dbd506bce957c2aeb11ad038ab2ed7a6719b7c8
Bryant Richards <dragonseyeimages@gmail.com>
2019-08-28 23:29:37 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -6,12 +6,14 @@ end
 
 def monopoly_with_second_tier
   monopoly = {
-    :railroads => {:pieces => {}}
+    :railroads => {pieces}
   }
 end
 
 def monopoly_with_third_tier
-  
+  monopoly = {
+    :railroads => {pieces: 4}
+  }
 end
 
 def monopoly_with_fourth_tier
